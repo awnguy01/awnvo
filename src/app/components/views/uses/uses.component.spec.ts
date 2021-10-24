90e6896df14e90427b4d1e487d5c3a078e16e101
Andrew Nguyen Vo <awnguy01@louisville.edu>
2021-10-23 21:00:57 -0400

    Upgrade to Angular v11

--- src/app/components/views/uses/uses.component.spec.ts
+++ src/app/components/views/uses/uses.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { UsesComponent } from './uses.component';
 
@@ -6,7 +6,7 @@ describe('UsesComponent', () => {
   let component: UsesComponent;
   let fixture: ComponentFixture<UsesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ UsesComponent ]
     })
